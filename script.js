f7a0971f53d5640e5aac413927cf04a73dcbd350
afartachury-max <afart.achury@gmail.com>
2026-09-12 19:22:26 -0400

    Update script.js

--- script.js
+++ script.js
@@ -11,3 +11,28 @@ function abrirSeccion(evt, nombreSeccion) {
   document.getElementById(nombreSeccion).classList.add('active');
   evt.currentTarget.classList.add('active');
 }
+import PhotoSwipeLightbox from 'https://cdnjs.cloudflare.com/ajax/libs/photoswipe/5.3.7/photoswipe-lightbox.esm.min.js';
+
+// Esperar a que el DOM esté cargado
+document.addEventListener('DOMContentLoaded', () => {
+
+  const lightbox = new PhotoSwipeLightbox({
+    // Selector para las galerías de imágenes
+    gallery: '.galeria',
+
+    // Selector para los elementos que abrirán el lightbox
+    children: 'a.lightbox-link',
+
+    // Opciones para la funcionalidad de zoom
+    initialZoomLevel: 'fit', // Ajustar imagen a la pantalla inicialmente
+    secondaryZoomLevel: 2, // Nivel de zoom secundario (al hacer doble clic/tap)
+    maxZoomLevel: 4, // Nivel máximo de zoom
+
+    // Módulo de PhotoSwipe core
+    pswpModule: () => import('https://cdnjs.cloudflare.com/ajax/libs/photoswipe/5.3.7/photoswipe.esm.min.js')
+  });
+
+  // Inicializar el lightbox
+  lightbox.init();
+
+});
